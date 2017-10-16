a7ad84d111abd505fe8ae9e72fd1065fb0a0442b
Kurvin Hendricks <Kurvin Hendricks>
2017-10-16 21:02:52 +0200

    Playing around with moment.js, getting basic js functions up and running

--- run.js
+++ run.js
@@ -5,7 +5,7 @@
 	window.console.log(checkCurrentTime());
 	window.console.log(getTwoHoursFromNow());
 	window.console.log(getThirtyMinutesFromNow());
-	window.console.log(addThirtyMinutesToPresent());
+	window.console.log(getTomorrow());
 
 })();
 
@@ -18,13 +18,35 @@ function checkCurrentTime() {
 }
 
 function getTwoHoursFromNow() {
-	return moment().hour() + 2;
+	var new_date = moment(moment().format("DD-MM-YYYY HH:mm:ss"), "DD-MM-YYYY HH:mm:ss").add(2, 'h');
+
+	var hour = new_date.format('HH');
+	var min = new_date.format('mm');
+	var day = new_date.format('DD');
+	var month = new_date.format('MM');
+	var year = new_date.format('YYYY');
+
+	return hour + ':' + min;
 }
 
 function getThirtyMinutesFromNow() {
-	return moment().minute() + 30;
+	var new_date = moment(moment().format("DD-MM-YYYY HH:mm:ss"), "DD-MM-YYYY HH:mm:ss").add(30, 'm');
+
+	var hour = new_date.format('HH');
+	var min = new_date.format('mm');
+	var day = new_date.format('DD');
+	var month = new_date.format('MM');
+	var year = new_date.format('YYYY');
+
+	return hour + ':' + min;
 }
 
-function addThirtyMinutesToPresent() {
-	return moment().add(30, 'm');
+function getTomorrow() {
+	var new_date = moment(moment().format("DD-MM-YYYY"), "DD-MM-YYYY").add(1, 'days');
+
+	var day = new_date.format('DD');
+	var month = new_date.format('MM');
+	var year = new_date.format('YYYY');
+
+	return day + ':' + month + ':' + year;
 }
