c219efa719167ab13268d58c3bb4193d6b743cc6
kurvinhendricks <kurvin@klarity.co.za>
2017-10-18 11:24:04 +0200

    Removing white space, updating functinoality and testing

--- run.js
+++ run.js
@@ -2,11 +2,12 @@
 
 	debug('wait a minute');
 
-	window.console.log(checkCurrentTime());
+	window.console.log(getCurrentTime());
 	window.console.log(getTwoHoursFromNow());
 	window.console.log(getThirtyMinutesFromNow());
-	window.console.log(getTomorrow());
+	window.console.log(getTomorrow('DD-MM-YYYY'));
 	window.console.log(returnTimeWithIntervals('09:30', 30));
+	window.console.log(checkIsToday('18-10-2017', 'DD-MM-YYYY'));
 
 })();
 
@@ -14,32 +15,57 @@ function debug(everything) {
 	console.log(everything);
 }
 
-function checkCurrentTime() {
+/*
+returns current date
+*/
+function getCurrentTime() {
 	return moment().format();
 }
 
+/*
+returns current time, plus two hours
+*/
 function getTwoHoursFromNow() {
 	var newDate = moment(moment().format("DD-MM-YYYY HH:mm:ss"), "DD-MM-YYYY HH:mm:ss").add(2, 'h');
-
 	return newDate.format('HH') + ':' + newDate.format('mm');
 }
 
+/*
+returns current time, plus thirty minutes
+*/
 function getThirtyMinutesFromNow() {
 	var newDate = moment(moment().format("DD-MM-YYYY HH:mm:ss"), "DD-MM-YYYY HH:mm:ss").add(30, 'm');
-
 	return newDate.format('HH') + ':' + newDate.format('mm');
 }
 
-function getTomorrow() {
-	var newDate = moment(moment().format("DD-MM-YYYY"), "DD-MM-YYYY").add(1, 'days');
-
-	return newDate.format('DD') + ':' + newDate.format('MM') + ':' + newDate.format('YYYY');
+/*
+requires date format
+returns tomorrow
+*/
+function getTomorrow($format) {
+	var newDate = moment(moment().format($format), $format).add(1, 'days');
+	return newDate.format('DD') + '-' + newDate.format('MM') + '-' + newDate.format('YYYY');
 }
 
+/*
+requires a date,
+checks if equal to todays date
+returns boolean value
+*/
 function returnTimeWithIntervals($startTime, $interval) {
 	var $returnTime = moment($startTime, 'HH:mm').add($interval, 'm');
-
 	return $returnTime.format('HH') + ':' + $returnTime.format('mm');
+}
+
+/*
+requires a date,
+checks if equal to todays date
+returns boolean value
+*/
+function checkIsToday($date, $format) {
+	var today = moment().format($format);
+	return today === $date;
+
 }
 
 /*
